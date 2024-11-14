81f6dd3790ff799deaff2a8889d95dfd5a36d937
Arap Bett <kipkorirjob@kabarak.ac.ke>
2024-11-14 16:19:53 -0500

    Create contact.php

---
+++ Front-End/contact.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+
+  // You can add code here to send an email or save the data to a database
+  $to = "bettarap254@gmail.com";
+  $subject = "New Contact Form Submission";
+  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Message sent successfully!";
+  } else {
+    echo "Failed to send message.";
+  }
+}
+?>
